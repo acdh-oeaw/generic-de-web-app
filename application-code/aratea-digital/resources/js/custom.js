40031f1f42112a4bc3a5300ddfa4efe9cd8d0162
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2016-10-31 14:05:11 +0100

    initial commit for extended features app

---
+++ application-code/aratea-digital/resources/js/custom.js
@@ -0,0 +1,10 @@
+/*https://www.sitepoint.com/url-parameters-jquery/*/
+$.urlParam = function(name){
+    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    if (results==null){
+       return null;
+    }
+    else{
+       return results[1] || 0;
+    }
+}
